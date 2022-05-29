748aad7aaf2b683e5c8de8d451e0f1b67db3157f
Nikki <Nikki@DESKTOP-L1609TP>
2022-05-30 00:13:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            options();
+        }
+        static void options()
+        {
+            triangle tri_question = new triangle();
+
+            Console.WriteLine("1) Work our Perimeter");
+            Console.WriteLine("2) Work out Area");
+            Console.WriteLine("3) Exit");
+
+            Console.WriteLine("--------------------------");
+
+            while (true)
+            {
+                wrong_option:
+                Console.WriteLine("Enter Choice");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice < 1 || choice >= 4)
+                {
+                    Console.WriteLine("invalid Input");
+                    goto wrong_option;
+                }
+                else if (choice == 1)
+                {
+                    Console.WriteLine("work out Perimeter");
+                    tri_question.tri_perimeter();
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("You want to work out the area of triangle.");
+                    tri_question.tri_area();
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("You want to exit");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace list
+{
+    class triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+
+        public void tri_perimeter()
+        {
+            Console.WriteLine(@"|\              ");
+            Console.WriteLine(@"| \             ");
+            Console.WriteLine(@"|  \            ");
+            Console.WriteLine(@"|a  \c           ");
+            Console.WriteLine(@"|    \          ");
+            Console.WriteLine(@"|     \         ");
+            Console.WriteLine(@"|___b__\        ");
+
+            Console.WriteLine("Work out the perimeter");
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("Enter a");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b");
+            b = double.Parse(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine("C:" + c);
+
+            double total_perimeter = a + b + c;
+
+            Console.WriteLine("Total perimeter:" + total_perimeter);
+        }
+        public void tri_area()
+        {
+            Console.WriteLine(@"|\              ");
+            Console.WriteLine(@"| \             ");
+            Console.WriteLine(@"|  \            ");
+            Console.WriteLine(@"|a  \c           ");
+            Console.WriteLine(@"|    \          ");
+            Console.WriteLine(@"|     \         ");
+            Console.WriteLine(@"|___b__\        ");
+
+            Console.WriteLine("Work out the Area");
+
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("Enter a");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b");
+            int b = int.Parse(Console.ReadLine());
+
+            double total_area = 0.5 * a * b;
+
+            Console.WriteLine("Total area:" + total_area);
+        }
+    }
+}
